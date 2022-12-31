71f0aab859d0f6508159bc4ec55291646ffe7893
Paul Frische <paul@paulfrische.xyz>
2022-12-31 12:51:31 +0100

    setup basic templating

--- src/app/views.py
+++ src/app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+def home(request: HttpRequest) -> HttpResponse:
+    return render(request, 'home.html', { 'title': 'home' })
+
